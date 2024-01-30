9b8abc090682cad347398153487fa87fdcdc5d17
Gustavo Fernandes de Moraes <pcvgustavo@gmail.com>
2024-01-30 16:42:30 -0300

    render user form, user list interface with render_template and url_for.

---
+++ database/user.py
@@ -0,0 +1,8 @@
+USERS = [
+    {"id":1, "name": "Gustavo", "email": "gustavo@test.com"},
+    {"id":2, "name": "Guilherme", "email": "guilherme@test.com"},
+    {"id":3, "name": "Joao", "email": "joao@test.com"},
+    {"id":4, "name": "Maria", "email": "maria@test.com"},
+    {"id":5, "name": "Jose", "email": "jose@test.com"},
+    {"id":6, "name": "Paulo", "email": "paulo@test.com"},
+]
